b3c0351041e1586b57e0bbcf585d723753e8fd93
bobytudu <34004403+bobytudu@users.noreply.github.com>
2024-01-04 20:59:25 +0530

    added new components

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/auth.reducer";
+import snackReducer from "./reducers/snack.reducer";
+import loadingReducer from "./reducers/loading.reducer";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    snack: snackReducer,
+    loading: loadingReducer,
   },
 });
 
